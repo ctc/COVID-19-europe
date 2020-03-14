dc4de1eb26589368bf21b8465bfeee3f9937d8ed
Ingo Flaschberger <ingo.flaschberger@gmail.com>
2020-03-14 23:24:54 +0100

    Sat Mar 14 23:24:54 CET 2020

--- scripts/update.sh
+++ scripts/update.sh
@@ -18,19 +18,19 @@ ${WORK_DIR}/scripts/reformat.py ${WORK_DIR}
 cd ${WORK_DIR}
 /usr/bin/git add total_confirmed.csv
 /usr/bin/git add total_deaths.csv
-/usr/bin/git add total_grow_confirmed.csv
-/usr/bin/git add total_grow_deaths.csv
+/usr/bin/git add total_recovered.csv
+
 /usr/bin/git add total_grow_percent_confirmed.csv
 /usr/bin/git add total_grow_percent_deaths.csv
 /usr/bin/git add total_grow_percent_recovered.csv
-/usr/bin/git add total_grow_recovered.csv
-/usr/bin/git add total_recovered.csv
+
 /usr/bin/git add by_population_confirmed.csv
 /usr/bin/git add by_population_deaths.csv
+/usr/bin/git add by_population_recovered.csv
+
 /usr/bin/git add by_population_grow_percent_confirmed.csv
 /usr/bin/git add by_population_grow_percent_deaths.csv
 /usr/bin/git add by_population_grow_percent_recovered.csv
-/usr/bin/git add by_population_recovered.csv
 
 /usr/bin/git add austria.csv
 
